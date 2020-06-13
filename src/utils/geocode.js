c43d09cc5b9c20e80da79d960f4b6dad6cfc1cba
pranav shankar <candypranav@gmail.com>
2020-06-13 14:58:45 +0530

    trial

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -22,5 +22,12 @@ const geocode = (address,callback)=>{
     })
 
 }
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: lat, lng: long },
+    zoom: 8
+  });
+}
 
 module.exports=geocode
